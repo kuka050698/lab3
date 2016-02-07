8a61946c140362c2bb896d90cac7f026b0d30b19
Yessenzhanov Kuanysh <kuanysh_05_98@mail.ru>
2016-02-07 15:42:02 +0600

    Added example of snake

---
+++ lab3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake1
+{
+    public class Food : Drawer
+    {
+        public Food()
+        {
+            color = ConsoleColor.Yellow;
+            sign = '$';
+        }
+    }
+}
+ 
